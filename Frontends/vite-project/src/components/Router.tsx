e62106ad3ec7a67dfbda3a561f323363f61a9af6
Your Name <your_email@example.com>
2024-08-16 15:29:17 +0530

    write_the_code

--- Frontends/vite-project/src/components/Router.tsx
+++ Frontends/vite-project/src/components/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home"
+import About from "./About"
 function Router() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
                 </Routes>
             </BrowserRouter>
 
