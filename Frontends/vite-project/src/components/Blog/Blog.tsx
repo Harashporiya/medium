afe2e110e85ed01e9d58b0e1eea9d242d2ede5ce
Your Name <your_email@example.com>
2024-08-17 12:00:14 +0530

    add_blog

--- Frontends/vite-project/src/components/Blog/Blog.tsx
+++ Frontends/vite-project/src/components/Blog/Blog.tsx
@@ -1,19 +1,26 @@
-import { UseBlog } from "../hooks/UseBlogs"
+import { UseBlog } from "../hooks/UseBlogs";
 import { useParams } from "react-router-dom";
 import FullBlog from "./FullBlog";
+
+
 function Blog() {
-    const { id } = useParams()
-    const { loading, blogs } = UseBlog({id:id || ""});
-    if(loading){
-        return <div>
-            loading...
-        </div>
+    const { id } = useParams();
+    const { loading, blogs } = UseBlog({ id: id || "" });
+
+    if (loading) {
+        return <div>loading...</div>;
     }
-  return (
-    <div>
-      <FullBlog blogs={blogs}/>
-    </div>
-  )
+
+   
+    if (blogs && typeof blogs === 'object') {
+        return (
+            <div>
+                <FullBlog blogs={blogs} />
+            </div>
+        );
+    } else {
+        return <div>No blog found.</div>;
+    }
 }
 
-export default Blog
+export default Blog;
