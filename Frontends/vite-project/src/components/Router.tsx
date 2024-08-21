a579faa31d94e7a623050916e677202128feb482
Your Name <your_email@example.com>
2024-08-21 20:27:32 +0530

    Complete_authorization

--- Frontends/vite-project/src/components/Router.tsx
+++ Frontends/vite-project/src/components/Router.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./Home"
-import About from "./About"
-import Signup from "./Signup/Signup"
-import Signin from "./Signin/Signin"
-import Blogs from "./Blog/Blogs"
-import Nav from "./Navbar/Nav"
-import Blog from "./Blog/Blog"
-import Publish from "./Publish"
-import Profile from "../Pages/Profile"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Signup from "./Signup/Signup";
+import Signin from "./Signin/Signin";
+import Blogs from "./Blog/Blogs";
+import Nav from "./Navbar/Nav";
+import Blog from "./Blog/Blog";
+import Publish from "./Publish";
+import Profile from "../Pages/Profile";
+import PrivateRoute from "../components/PrivateRoute";
+
 function Router() {
     return (
         <div>
@@ -17,16 +19,37 @@ function Router() {
                     <Route path="/about" element={<About/>}/>
                     <Route path="/signup" element={<Signup/>}/>
                     <Route path="/signin" element={<Signin/>}/>
-                    <Route path="/blogs" element={<Blogs/>}/>
-                    <Route path="/nav" element={<Nav/>}/>
-                    <Route path="/blog/:id" element={<Blog/>}/>
-                    <Route path="/publish" element={<Publish/>}/>
-                    <Route path="/profile/:userId" element={<Profile/>}/>
+                    
+                   
+                    <Route path="/blogs" element={
+                        <PrivateRoute>
+                            <Blogs />
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/nav" element={
+                        <PrivateRoute>
+                            <Nav />
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/blog/:id" element={
+                        <PrivateRoute>
+                            <Blog />
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/publish" element={
+                        <PrivateRoute>
+                            <Publish />
+                        </PrivateRoute>
+                    }/>
+                    <Route path="/profile/:userId" element={
+                        <PrivateRoute>
+                            <Profile />
+                        </PrivateRoute>
+                    }/>
                 </Routes>
             </BrowserRouter>
-
         </div>
     )
 }
 
-export default Router
+export default Router;
